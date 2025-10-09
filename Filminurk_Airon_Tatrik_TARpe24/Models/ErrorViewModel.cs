babafe415ee27d058a3d79a1518d991431fceb14
Opilane TTHK <116342432+xZtsu@users.noreply.github.com>
2025-10-09 10:21:22 +0300

    filminurk

---
+++ Filminurk_Airon_Tatrik_TARpe24/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Filminurk_Airon_Tatrik_TARpe24.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
