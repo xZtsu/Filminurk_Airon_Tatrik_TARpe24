1a56aa049e236340c0019006b4dfbf178ab6f03d
Opilane TTHK <opilane@tthk.ee>
2025-12-05 12:03:26 +0200

    added env example, for email sending connection data security.  old passwords you find in this repo wont work

---
+++ Filminurk.Data/EnvironmentExample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Data
+{
+    public class EnvironmentExample
+    {
+        public static string smtaddress = ""; //server address for sending emails via smtp
+        public static string gmailapppassword = ""; //password for user, that has access to this server(google app password)
+        public static string gmailusername = "";//username of email sending user (gmail account).
+    }
+}
