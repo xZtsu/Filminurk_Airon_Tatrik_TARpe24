375c37391d9c3c454922b8e846b191a196de0eea
xZtsu <116342432+xZtsu@users.noreply.github.com>
2026-02-05 20:08:21 +0200

    Username instead of Email jututuba

--- Filminurk_Airon_Tatrik_TARpe24/Hubs/ChatHub.cs
+++ Filminurk_Airon_Tatrik_TARpe24/Hubs/ChatHub.cs
@@ -1,15 +1,28 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Filminurk.Core.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
+using Filminurk.ApplicationServices.Services;
 using System;
+
 namespace Filminurk.Hubs
 {
     public class ChatHub : Hub
     {
-        [Authorize] //only logged in users
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ChatHub(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Authorize] 
         public async Task SendMessage(string message)
         {
-            var userName = Context.User?.Identity?.Name ?? "Unknown";
-            await Clients.All.SendAsync("ReceiveMessage", userName, message);
+            var user = await _userManager.GetUserAsync(Context.User);
+            var displayName = user?.DisplayName ?? user?.UserName ?? "Unknown";
+
+            await Clients.All.SendAsync("ReceiveMessage", displayName, message);
         }
     }
 }
